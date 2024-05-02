1e1b5894d6f3b34fe16995601339438621203dbb
Slava Krokhmal <krohmalvyacheslav@yandex.ru>
2024-05-02 21:59:20 +0300

    Выполнить требования ТЗ 6 спринта + исправление CodeStyle

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -2,17 +2,30 @@ import java.util.List;
 
 public interface TaskManager {
     void createTask(Task task);
+
     void updateTask(Task task);
+
     Task getTaskById(int id);
+
     List<Task> getAllTasks();
+
     List<Epic> getAllEpics();
+    
     List<Subtask> getAllSubtasks();
+
     void deleteTask(int id);
+
     void deleteAllEpics();
+
     void deleteAllTasks();
+
     void deleteAllSubtasks();
+
     Task getTask(int id);
+
     Epic getEpic(int id);
+
     Subtask getSubtask(int id);
+
     List<Task> getHistory();
 }
