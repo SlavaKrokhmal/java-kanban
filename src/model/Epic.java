410a16e086a342a840aeef684d952aa1febe79fa
Slava Krokhmal <krohmalvyacheslav@yandex.ru>
2024-06-14 00:50:13 +0300

    Решение ТЗ 8 спринта

--- src/model/Epic.java
+++ src/model/Epic.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.time.LocalDateTime;
 import java.time.Duration;
-import java.util.Comparator;
 
 public class Epic extends Task {
     private List<Subtask> subtasks;
